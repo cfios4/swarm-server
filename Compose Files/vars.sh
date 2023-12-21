e300ec7ba3793b198a2f5fd2c4ded1dd4bf24a77
cafio <62921651+cfios4@users.noreply.github.com>
2023-12-20 19:51:13 -0500

    create needed folders, fixed docker networks for macvlan

--- Compose Files/vars.sh
+++ Compose Files/vars.sh
@@ -8,10 +8,10 @@ export SUBNET=192.168.45.0/255.255.255.0
 export TAILSCALEIP=$(tailscale ip -4)
 export PLEX_CLAIM=$(docker exec $(docker ps -f name=plex --format "{{.ID}}") sh -c 'curl -s "https://plex.tv/api/claim/token?X-Plex-Token=$(grep PlexOnlineToken config/Library/Application\ Support/Plex\ Media\ Server/Preferences.xml | cut -d '\'' '\'' -f 4 | cut -d '\''"'\'' -f 2)" | cut -d '\''"'\'' -f 2')
 
-mkdir -p $APPDATA_MNT/{traefik,flame,gitea,nextcloud,postgres,vaultwarden,vscode,plex,radarr,sabnzbd,sonarr,}
+mkdir -p $CLUSTER_MNT/{media,appdata/{traefik,flame,gitea,nextcloud,postgres,vaultwarden,vscode,plex,radarr,sonarr,sabnzbd}}
 
-docker network create --config-only --subnet 192.168.45.0/24 -o parent=enp6s18 --ip-range=192.168.45.254/32 agh-ip
-docker network create -d macvlan --scope swarm --attachable --config-from agh-ip macvlan4home
+docker network create --config-only -o parent=enp6s18 --subnet 192.168.45.0/24 --gateway 192.168.45.1 --ip-range=192.168.45.254/32 dns-ip
+docker network create -d macvlan --scope swarm --attachable --config-from dns-ip macvlan4home
 
 ##################### NOTES ###########################
 ## glusterfs is probably the best way to pool storage
