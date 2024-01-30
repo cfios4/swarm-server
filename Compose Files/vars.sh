de5fe1a9c06e1eff91d3aaa74ac371593c2b9517
swarm1 <swarm1@cafio.co>
2024-01-29 20:05:15 -0500

    password as variable maybe more idk

--- Compose Files/vars.sh
+++ Compose Files/vars.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export APPDATA_MNT=/appdata
 export MEDIA_MNT=/media
+export PASSWORD=Admin!!1
 export DOMAIN=cafio.co
 export TZ=America/New_York
 export NETBIRDIP=$(netbird status --ipv4)
