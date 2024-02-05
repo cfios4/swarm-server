d3baaed3583b3a38bec0d79a6abc3487894d1355
cafio <62921651+cfios4@users.noreply.github.com>
2024-02-04 21:02:00 -0500

    vars need exporting

--- Compose Files/deploy.sh
+++ Compose Files/deploy.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-APPDATA_MNT=~/appdata
-MEDIA_MNT=~/media
-DOMAIN=cafio.co
-PASSWORD=CHANGEME
-TZ=America/New_York
-NETBIRDIP=$(netbird status --ipv4)
-PLEX_CLAIM= #https://plex.tv/claim
+export APPDATA_MNT=/mnt/gluster/appdata
+export MEDIA_MNT=/mnt/gluster/media
+export DOMAIN=cafio.co
+export PASSWORD=CHANGEME
+export TZ=America/New_York
+export NETBIRDIP=$(netbird status --ipv4)
+export PLEX_CLAIM= #https://plex.tv/claim
 
-GATEWAY=$(ip route | grep default | awk '{print $3}')
-DNS=$(docker network inspect macvlan4home -f '{{range .IPAM.Config}}{{.IPRange}}{{end}}' | awk -F/ '{print $1}')
-NETWORK=$(ip a | grep -E 'enp|eth|eno' | grep -m 1 inet | awk '{print $2}' | awk -F'/' '{print $1}' | awk -F'.' '{print $1"."$2"."$3}')
+export GATEWAY=$(ip route | grep default | awk '{print $3}')
+export DNS=$(docker network inspect macvlan4home -f '{{range .IPAM.Config}}{{.IPRange}}{{end}}' | awk -F/ '{print $1}')
+export NETWORK=$(ip a | grep -E 'enp|eth|eno' | grep -m 1 inet | awk '{print $2}' | awk -F'/' '{print $1}' | awk -F'.' '{print $1"."$2"."$3}')
 
 
 docker stack up -c $1.yaml $1
