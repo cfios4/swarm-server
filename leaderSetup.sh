dc3c15b92047014684ab34de7cfb1bf1e89f6309
cafio <62921651+cfios4@users.noreply.github.com>
2024-02-04 21:01:28 -0500

    silence apt commands

--- leaderSetup.sh
+++ leaderSetup.sh
@@ -8,7 +8,7 @@ curl -fsSL https://get.docker.com | bash
 sudo usermod -aG sudo,docker swarm
 curl -fsSL https://pkgs.netbird.io/install.sh | bash
 netbird up -k $netbirdkey -n $nodename
-sudo apt update ; sudo apt upgrade -y
+sudo apt update ; sudo apt upgrade -y > /dev/null > 2>&1
 sudo reboot
 
 ### LEADER ONLY (as swarm)
@@ -20,11 +20,9 @@ worker_token=$(docker swarm join-token worker -q)
 
 ssh_key="$HOME/.ssh/id_rsa"
 if [ ! -f "$ssh_key" ]; then
-    echo "Generating SSH key pair..."
     ssh-keygen -t rsa -b 4096 -f "$ssh_key"
 fi
 for node in ${cluster[@]} ; do
-    echo "Copying public key to $node..."
     ssh-copy-id -i "$ssh_key.pub" "$node"
 done
 
@@ -44,10 +42,10 @@ echo Admin!!1 | sudo -sS
 sudo -s
 mkdir -vp /mnt/gluster{/appdata,/media,/bricks}
 
-# apt update ; apt install -y glusterfs-server
-# systemctl enable glusterd ; systemctl start glusterd
+apt update ; apt install -y glusterfs-server > /dev/null > 2>&1
+systemctl enable glusterd ; systemctl start glusterd
 echo "Formatting external storage on $node..."
-sudo mkfs.ext4 /dev/nvme0n1
+sudo mkfs.ext4 /dev/nvme0n1 > /dev/null > 2>&1
 echo '/dev/nvme0n1 /mnt/gluster/bricks ext4 defaults 0 0' | sudo tee -a /etc/fstab
 echo "Mounting external storage on $node..."
 mkdir -p /mnt/gluster/bricks/{appdata,media}
@@ -84,7 +82,7 @@ for node in $(docker node ls --filter "role=manager" --format "{{.Hostname}}") ;
     ssh swarm@$node.netbird.cloud bash <<SSH
 echo Admin!!1 | sudo -sS
 sudo -s
-sudo apt update ; sudo apt install ipcalc-ng ; sudo apt remove ipcalc
+sudo apt update ; sudo apt install ipcalc-ng ; sudo apt remove ipcalc > /dev/null > 2>&1
 echo "Creating Docker network for DNS on $node..."
 docker network create --config-only -o parent=\$(ip link show | grep -Po '^\d+: \K(eth|eno|enp)[^:]+') --subnet \$(echo \$(ipcalc-ng -n \$(ip a | grep -E 'enp|eth|eno' | grep inet | awk '{print \$2}') --no-decorate)/\$(ipcalc-ng -p \$(ip a | grep -E 'enp|eth|eno' | grep inet | awk '{print \$2}') --no-decorate)) --gateway \$(ip route | grep default | awk '{print \$3}') --ip-range=\$(ip a | grep -E 'enp|eth|eno' | grep inet | awk '{print \$2}' | awk -F / '{print \$1}' | awk -F . '{print \$1"."\$2"."\$3}').254/32 macvlan4home
 docker network create -d macvlan --scope swarm --attachable --config-from macvlan4home dns-ip
