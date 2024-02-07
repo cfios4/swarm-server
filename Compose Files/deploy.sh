4bb97d6ba3ca6d0fdc46ce72375a14e95d1bdee2
casey <casey@yoga.lan>
2024-02-06 21:00:23 -0500

    ask for important variables

--- Compose Files/deploy.sh
+++ Compose Files/deploy.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
+read -p "Password for apps (Flame, Nextcloud, ...)" PASSWORD
+read -p "Plex claim ID (https://plex.tv/claim): " PLEX_CLAIM
+export $PASSWORD
+export $PLEX_CLAIM
 export APPDATA_MNT=/mnt/gluster/appdata
 export MEDIA_MNT=/mnt/gluster/media
 export DOMAIN=cafio.co
-export PASSWORD=CHANGEME
 export TZ=America/New_York
 export NETBIRDIP=$(netbird status --ipv4)
-export PLEX_CLAIM= #https://plex.tv/claim
 
 export GATEWAY=$(ip route | grep default | awk '{print $3}')
 export DNS=$(docker network inspect macvlan4home -f '{{range .IPAM.Config}}{{.IPRange}}{{end}}' | awk -F/ '{print $1}')
