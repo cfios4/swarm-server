646a08a7530a78f68f732263434d9a1963fb8361
casey <casey@cafio.co>
2024-02-13 00:22:43 +0000

    uncap

---
+++ compose files/deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+until [ ! -z "$PASSWORD" ] ; do
+	echo "\$PASSWORD is not set"
+	read -p "Set admin password for apps (Flame, Nextcloud, ...): " PASSWORD
+	export PASSWORD
+done
+
+read -p "Plex claim ID (https://plex.tv/claim): " PLEX_CLAIM
+export PLEX_CLAIM
+export APPDATA_MNT=/mnt/gluster/appdata
+export MEDIA_MNT=/mnt/gluster/media
+export DOMAIN=cafio.co
+export TZ=America/New_York
+export NETBIRDIP=$(netbird status --ipv4)
+
+export GATEWAY=$(ip route | grep default | awk '{print $3}')
+export DNS=$(docker network inspect macvlan4home -f '{{range .IPAM.Config}}{{.IPRange}}{{end}}' | awk -F/ '{print $1}')
+export NETWORK=$(ip a | grep -E 'enp|eth|eno' | grep -m 1 inet | awk '{print $2}' | awk -F'/' '{print $1}' | awk -F'.' '{print $1"."$2"."$3}')
+
+
+docker stack up -c $1 $(basename $1 .yaml)
