30974ed4702aa6d1da1e59cb30019cfeba9a1e7b
casey <casey@cafio.co>
2024-04-09 22:07:10 +0000

    add sleep and mount to rc.local because fstab isn't working with gluster

--- swarm.sh
+++ swarm.sh
@@ -138,6 +138,25 @@ function final_touches () {
     echo 'DNSStubListener=no' >> /etc/systemd/resolved.conf
     rm /etc/resolv.conf && ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf
 
+    cat <<EOF > /etc/rc.local
+#!/bin/sh -e
+#
+# rc.local
+#
+# This script is executed at the end of each multiuser runlevel.
+# Make sure that the script will "exit 0" on success or any other
+# value on error.
+#
+# In order to enable or disable this script just change the execution
+# bits.
+#
+# By default this script does nothing.
+
+sleep 5 && mount -a 
+
+exit 0
+EOF
+
     reboot
 }
 
